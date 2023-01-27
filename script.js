cf5a11df41329e262239caac8a1f8bf4294f6b95
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-01-26 22:24:42 -0800

    Added Image Icons and Text for Temp, Wind and Humidity

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function savedSearch(city) {
     ) {
         previousSearches.push(city)
         localStorage.setItem("searchedcitys", JSON.stringify(previousSearches))
+        displayButtons()
     }
 }
 
@@ -59,40 +60,59 @@ function getWeather(lat, lon) {
 }
 
 
-/*var day5 = document.getElementById("buttons")
-var anotherP = document.createElement("p");
-anotherP.textContent= "hello world";
-day5.appendChild(anotherP);
-anotherP.style.color="red";*/
-
 function displayWeather(days5) {
 
+    
     const days5Container = document.getElementById("day5-forecast");
+    days5Container.innerHTML = ""
     days5Container.classList.add("days")
+
     for (let index = 0; index < days5.length; index++) {
         const day = days5[index];
+
         const dEl = document.createElement("div")
         dEl.classList.add("day")
         dEl.textContent = day.dt_txt.replace(" 12:00:00", "");
         days5Container.append(dEl);
 
         const temp = document.createElement("h3")
-        temp.textContent = day.main.temp
+        temp.textContent = "Temperature: " + day.main.temp
         dEl.appendChild(temp);
 
         const wind = document.createElement("h3")
-        wind.textContent = day.wind.speed
+        wind.textContent ="Wind Speed: " + day.wind.speed
         dEl.appendChild(wind)
 
         const humid = document.createElement("h3")
-        humid.textContent = day.main.humidity
+        humid.textContent ="Humidity: " + day.main.humidity
         dEl.appendChild(humid)
 
-    }
-
-
+        const weatherImages = document.createElement("img")
+        weatherImages.setAttribute("src", `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`)
+        dEl.appendChild(weatherImages)
+    } 
 
 }
+
+
+function displayButtons(){
+ const savedCities = JSON.parse(localStorage.getItem("searchedcitys"))
+ const buttonDiv = document.getElementById("buttons");
+ buttonDiv.innerHTML = ""
+ for (let i = 0; i < savedCities.length; i++) {
+    const button = document.createElement("button");
+    button.textContent = savedCities[i]
+    
+    buttonDiv.append(button);
+    button.addEventListener("click", function(event){
+       event.preventDefault()
+    getLocation(savedCities[i])
+    })
+    
+ }
+}
+
+displayButtons();
 
 
 function currentWeather(days1) {
@@ -102,6 +122,9 @@ function currentWeather(days1) {
     day1Container.innerHTML = ""; //set on other functions above
     day1Weather.classList.add("day1")
     const date = new Date(days1.dt * 1000)
+    const  image = document.createElement("img")
+    image.setAttribute("src", `https://openweathermap.org/img/wn/${days1.weather[0].icon}.png`)
+    day1Container.append(image)
 
 
     day1Weather.textContent = date.toLocaleDateString();
@@ -112,11 +135,11 @@ function currentWeather(days1) {
     day1Container.appendChild(temp)
 
     const wind = document.createElement("h3")
-    wind.textContent = days1.wind.speed
+    wind.textContent = "Wind Speed: " + days1.wind.speed
     day1Container.appendChild(wind)
 
     const humid = document.createElement("h3")
-    humid.textContent = days1.main.humidity
+    humid.textContent ="Humidity: " + days1.main.humidity
     day1Container.appendChild(humid)
 
     console.log(days1);
